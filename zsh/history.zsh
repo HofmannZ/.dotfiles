139390b5fe65a7f1efa3f71270d33a845799790a
Zino Hofmann <zino@hofmann.amsterdam>
2022-05-18 09:00:05 +0000

    feat: don't ignore dupes

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -7,5 +7,4 @@ setopt HIST_IGNORE_DUPS          # Do not record an event that was just recorded
 setopt HIST_IGNORE_ALL_DUPS      # Delete an old recorded event if a new event is a duplicate.
 setopt HIST_FIND_NO_DUPS         # Do not display a previously found event.
 setopt HIST_IGNORE_SPACE         # Do not record an event starting with a space.
-setopt HIST_SAVE_NO_DUPS         # Do not write a duplicate event to the history file.
 setopt HIST_VERIFY               # Do not execute immediately upon history expansion.
